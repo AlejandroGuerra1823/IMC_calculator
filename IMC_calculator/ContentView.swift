a14b41d691e9d7ee44f6b5bed6eb9bdf2beedc0f
Alejandro Guerra <guerramanuel299@gmail.com>
2024-08-20 20:55:52 -0500

    Initial Commit

---
+++ IMC_calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IMC_calculator
+//
+//  Created by Alejandro Guerra on 20/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, new pryect!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IMC_calculator/IMC_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMC_calculatorApp.swift
+//  IMC_calculator
+//
+//  Created by Alejandro Guerra on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct IMC_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
